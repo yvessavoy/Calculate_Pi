f149ad2c3d81ae7a1edaf82e3b67aec6baeb1311
Yves Savoy <10594588+yvessavoy@users.noreply.github.com>
2021-10-05 13:48:03 +0200

    Add a hardware timer and steer it through the user input and the calculation

--- U_Calculate_Pi/main.c
+++ U_Calculate_Pi/main.c
@@ -28,9 +28,11 @@
 
 #include "rtos_buttonhandler.h"
 
-#define EG_RUN_LEIB  0x01
-#define EG_RUN_NILA  0x02
-#define EG_SKIP_CALC 0x04
+#define EG_RUN_LEIB  (1 << 0)
+#define EG_RUN_NILA  (1 << 1)
+#define EG_SKIP_CALC (1 << 2)
+#define EG_TT		 (1 << 3)
+#define EG_RST_TIMER (1 << 4)
 
 #define PI_5DECIMALS 3.14159
 
@@ -44,30 +46,48 @@ typedef enum {
 	NILAKANTHA,
 } Algorithm_e;
 
-EventGroupHandle_t eventGroup;
+EventGroupHandle_t xEventGroup;
 Algorithm_e algorithm = LEIBNIZ;
 TaskHandle_t leibnizHandle;
 TaskHandle_t nilakanthaHandle;
 State_e state = State_Stopped;
 
 float pi;
+int seconds;
+int milliseconds;
 
 extern void vApplicationIdleHook(void);
 void vCalculateLeibniz(void *pvParameters);
 void vCalculateNilakantha(void *pvParameters);
 void vInterface(void *pvParameters);
 void vButtonHandler(void *pvParameters);
+void vTimeHandler(void *pvParameters);
+
+ISR(TCC1_OVF_vect) {
+	xEventGroupSetBitsFromISR(xEventGroup, EG_TT, pdFALSE);
+}
 
 void vApplicationIdleHook(void) {}
+	
+void vInitTimer() {
+	// Setup timer that interrupts every 10ms
+	// Disable it as it will be started in vButtonHandler()
+	TCC1.CTRLA = 0x00;
+	TCC1.CTRLB = 0x00;
+	TCC1.INTCTRLA = 0x03;
+	TCC1.PER = 5000;
+}
 
 int main(void) {
 	vInitClock();
 	vInitDisplay();
+	vInitTimer();
 	
-	eventGroup = xEventGroupCreate();
+	xEventGroup = xEventGroupCreate();
 	
 	xTaskCreate(vInterface, (const char *) "interface", configMINIMAL_STACK_SIZE + 50, NULL, 2, NULL);
 	xTaskCreate(vButtonHandler, (const char *) "buttonHandler", configMINIMAL_STACK_SIZE + 50, NULL, 2, NULL);
+	xTaskCreate(vTimeHandler, (const char *) "timeHandler", configMINIMAL_STACK_SIZE + 50, NULL, 2, NULL);
 	xTaskCreate(vCalculateLeibniz, (const char *) "calculateLeibniz", configMINIMAL_STACK_SIZE+10, NULL, 1, &leibnizHandle);
 	xTaskCreate(vCalculateNilakantha, (const char *) "calculateNilakantha", configMINIMAL_STACK_SIZE+10, NULL, 1, &nilakanthaHandle);
 	
@@ -76,8 +96,35 @@ int main(void) {
 	return 0;
 }
 
+void vTimeHandler(void *pvParameters) {
+	int i = 0;
+	seconds = 0;
+	milliseconds = 0;
+	
+	for (;;) {
+		xEventGroupWaitBits(xEventGroup, EG_TT, pdTRUE, pdTRUE, portMAX_DELAY);
+		
+		// Check if time reset is requested
+		if (xEventGroupClearBits(xEventGroup, EG_RST_TIMER) & EG_RST_TIMER) {
+			i = 0;
+			seconds = 0;
+			milliseconds = 0;
+		}
+		
+		if (i < 100) {
+			milliseconds += 10;
+			i++;
+		} else {
+			seconds++;
+			milliseconds = 0;
+			i = 0;
+		}
+	}
+}
+
 void vInterface(void *pvParameters) {
-	char cPi[8];
+	char cPi[15];
+	char cTime[14];
 	
 	for(;;) {
 		vDisplayClear();
@@ -85,15 +132,22 @@ void vInterface(void *pvParameters) {
 		
 		switch (state) {
 			case State_Started:
-				// Temporarily skip calculation while fetching PI value
-				xEventGroupSetBits(eventGroup, EG_SKIP_CALC);
+				// Temporarily skip calculation while fetching PI value and time
+				xEventGroupSetBits(xEventGroup, EG_SKIP_CALC);
 				
-				sprintf(cPi, "PI: %f", pi);
+				if (algorithm == LEIBNIZ) {
+					sprintf(cPi, "PI: %0.8f", pi * 4);
+				} else {
+					sprintf(cPi, "PI: %0.8f", pi);
+				}
 				
 				// Re-Enable calculation after fetching is complete
-				xEventGroupClearBits(eventGroup, EG_SKIP_CALC);
+				xEventGroupClearBits(xEventGroup, EG_SKIP_CALC);
+				
+				sprintf(cTime, "Time: %i.%is", seconds, milliseconds);
 				
 				vDisplayWriteStringAtPos(1, 0, cPi);
+				vDisplayWriteStringAtPos(2, 0, cTime);
 				vDisplayWriteStringAtPos(3, 0, "START STOP RST CHNG");
 				break;
 				
@@ -135,23 +189,30 @@ void vButtonHandler(void *pvParameters) {
 		// Start algorithm (means resuming the correct calculation task)
 		if(getButtonState(BUTTON1, true) == buttonState_Short && state == State_Stopped) {
 			if (algorithm == LEIBNIZ) {
-				xEventGroupSetBits(eventGroup, EG_RUN_LEIB);
+				xEventGroupSetBits(xEventGroup, EG_RUN_LEIB);
 			} else {
-				xEventGroupSetBits(eventGroup, EG_RUN_NILA);
+				xEventGroupSetBits(xEventGroup, EG_RUN_NILA);
 			}
 			
 			state = State_Started;
+			
+			// Start HW-Timer
+			TCC1.CTRLA = TC_CLKSEL_DIV64_gc;
 		}
 		
 		// Stop algorithm (means deleting the currently running calculation task)
 		if(getButtonState(BUTTON2, true) == buttonState_Short && state == State_Started) {
 			if (algorithm == LEIBNIZ) {
-				xEventGroupClearBits(eventGroup, EG_RUN_LEIB);
+				xEventGroupClearBits(xEventGroup, EG_RUN_LEIB);
 			} else {
-				xEventGroupClearBits(eventGroup, EG_RUN_NILA);
+				xEventGroupClearBits(xEventGroup, EG_RUN_NILA);
 			}
 			
 			state = State_Stopped;
+			
+			// Stop HW-Timer
+			TCC1.CTRLA = 0x00;
+			xEventGroupSetBits(xEventGroup, EG_RST_TIMER);
 		}
 		
 		// Reset algorithm
@@ -177,21 +238,29 @@ void vCalculateLeibniz(void *pvParameters) {
 	EventBits_t xEventGroupValue;
 	
 	for (;;) {
-		xEventGroupWaitBits(eventGroup, EG_RUN_LEIB, pdFALSE, pdTRUE, portMAX_DELAY);
-		pi = 4.0;
+		xEventGroupWaitBits(xEventGroup, EG_RUN_LEIB, pdFALSE, pdTRUE, portMAX_DELAY);
+		pi = 1.0;
 		i = 0;
 	
+		xEventGroupValue = xEventGroupGetBits(xEventGroup);
+		
 		// Exit the inner loop if EG_RUN_LEIB gets cleared
-		xEventGroupValue = xEventGroupGetBits(eventGroup);
 		while(xEventGroupValue & EG_RUN_LEIB) {
 			// If the display needs to fetch the current PI value, we stop
 			// the calculation temporarily to avoid incomplete float values
 			if ((xEventGroupValue & EG_SKIP_CALC) == 0) {
-				pi = pi - (4.0 / (3 + (4 * i))) + (4.0 / (5 + (4 * i)));
+				pi = pi - (1.0 / (3 + (4 * i))) + (1.0 / (5 + (4 * i)));
 				i++;
+				
+				// If algorithm calculated PI up to 5 decimal places,
+				// stop the timer
+				if ((pi * 4) - PI_5DECIMALS < 0.00001) {
+					TCC1.CTRLA = 0x00;
+					xEventGroupSetBits(xEventGroup, EG_RST_TIMER);
+				}
 			}
 			
-			xEventGroupValue = xEventGroupGetBits(eventGroup);
+			xEventGroupValue = xEventGroupGetBits(xEventGroup);
 		}
 	}
 }
@@ -201,12 +270,13 @@ void vCalculateNilakantha(void *pvParameters) {
 	EventBits_t xEventGroupValue;
 	
 	for(;;) {
-		xEventGroupWaitBits(eventGroup, EG_RUN_NILA, pdFALSE, pdTRUE, portMAX_DELAY);
+		xEventGroupWaitBits(xEventGroup, EG_RUN_NILA, pdFALSE, pdTRUE, portMAX_DELAY);
 		pi = 3.0;
 		i = 1;
 		
+		xEventGroupValue = xEventGroupGetBits(xEventGroup);
+		
 		// Exit the inner loop if EG_RUN_NILA gets cleared
-		xEventGroupValue = xEventGroupGetBits(eventGroup);
 		while(xEventGroupValue & EG_RUN_NILA) {
 			// If the display needs to fetch the current PI value, we stop
 			// the calculation temporarily to avoid incomplete float values
@@ -215,9 +285,16 @@ void vCalculateNilakantha(void *pvParameters) {
 				i++;
 				pi = pi - (4.0 / ((2 * i) * (2 * i + 1) * (2 * i + 2)));
 				i++;
+				
+				// If algorithm calculated PI up to 5 decimal places,
+				// stop the timer
+				if (pi - PI_5DECIMALS < 0.00001) {
+					TCC1.CTRLA = 0x00;
+					xEventGroupSetBits(xEventGroup, EG_RST_TIMER);
+				}
 			}
 		
-			xEventGroupValue = xEventGroupGetBits(eventGroup);
+			xEventGroupValue = xEventGroupGetBits(xEventGroup);
 		}
 	}
 }
